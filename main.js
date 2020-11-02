9557a0c831f097b1fc7806c266081cb5107ffc49
Arnavkheterpal12 <71542068+Arnavkheterpal12@users.noreply.github.com>
2020-11-02 19:39:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function take_snapshot(){
     });
 }
 console.log('ml5 version:', ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/AR9y4nls2/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/VrplLig2Q/',modelLoaded);
 function modelLoaded(){
     console.log('Model Loaded!');
 }
